0c5ca6edfbeddd460875f12983e3d7b99a762ca0
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2024-07-19 10:25:22 -0500

    No need for comms overlay rofl

--- Stream Tool/Resources/Scripts/Comms/CGlobals.mjs
+++
@@ -1,9 +0,0 @@
-// values in seconds
-export const fadeInTimeSc = .3;
-export const fadeOutTimeSc = .2;
-
-/**
- * All animations will get this delay when the html loads. 
- * Change this so it times with your transition
- */
-export const introDelaySc = .6;
